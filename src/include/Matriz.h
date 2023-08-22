fc368c01e43f61d3739b48163846d60f1a56cb8d
LeoNouz <leonardonouzinho13@gmail.com>
2023-08-22 19:17:35 -0300

    Versão 1.1.0 - Getters e Setters da classe Matriz

--- src/include/Matriz.h
+++ src/include/Matriz.h
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <random>
 using namespace std;
 
 class Matriz {
     public:
         Matriz(const int linhas, const int colunas);
         ~Matriz();
+
+        int get_linhas() const;
+        int get_colunas() const;
+        int** get_matriz() const;
+
+        void set_manual() const;
+        void set_auto() const;
 
         void mostra_matriz() const;
     private:
@@ -38,4 +46,44 @@ void Matriz::mostra_matriz() const {
         cout << endl;
     }
     cout << endl;
+}
+
+int Matriz::get_linhas() const {
+    return m_linhas;
+}
+
+int Matriz::get_colunas() const {
+    return m_colunas;
+}
+
+int** Matriz::get_matriz() const {
+    return m_matriz;
+}
+
+void Matriz::set_manual() const {
+    cout << endl;
+    for (int i = 0; i < m_linhas; i++) {
+        for (int j = 0; j < m_colunas; j++) {
+            cout << "Elemento [" << i << "][" << j << "]: ";
+            cin >> m_matriz[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void Matriz::set_auto() const {
+    // Crie um mecanismo de geração de números aleatórios (RNG)
+    std::random_device rd;
+    std::mt19937 gen(rd()); // Mersenne Twister, um algoritmo popular de geração de números aleatórios
+
+    // Crie uma distribuição de números aleatórios (por exemplo, de 1 a 100)
+    std::uniform_int_distribution<> dist(1, 99);
+
+    for (int i = 0; i < m_linhas; i++) {
+        for (int j = 0; j < m_colunas; j++) {
+            int random_number = dist(gen);
+            m_matriz[i][j] = random_number;
+        }
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    Matriz matriz_prototipo(10, 10);
-    matriz_prototipo.mostra_matriz();
+    Matriz prototipo(10, 10);
+    prototipo.mostra_matriz();
+
+    prototipo.set_auto();
+    prototipo.mostra_matriz();
+
     return 0;
 }
